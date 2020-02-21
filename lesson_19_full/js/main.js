725362dbb8015c5fac319e4de40a7d2a84b14108
s0ick <36438226+s0ick@users.noreply.github.com>
2020-02-21 22:49:20 +0300

    fix: menu

--- lesson_19_full/js/main.js
+++ lesson_19_full/js/main.js
@@ -52,11 +52,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     body.addEventListener('click', (event) => {
       let target = event.target;
-      if(target.classList.contains('.menu')) menu.classList.toggle('active-menu');
+      if(target.closest('.menu')) menu.classList.toggle('active-menu');
       else if(target.classList.contains('close-btn')) menu.classList.toggle('active-menu');
-      else if(target.matches('ul>li')) target.preventDefault();
-      else if(target.matches('a')) menu.classList.toggle('active-menu');
-      else if(!target.matches('menu')) menu.classList.toggle('active-menu');
+      else if(target.matches('ul>li')) event.preventDefault();
+      else if(target.matches('a') && !target.classList.contains('portfolio-btn')) menu.classList.toggle('active-menu');
+      else if(!target.matches('menu')) menu.classList.remove('active-menu');
     });
   };
   toggleMenu();
